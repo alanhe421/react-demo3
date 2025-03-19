53e1596662e71c1cc357c82878829718d8d8953a
Alan He <qianghe421@gmail.com>
2025-03-19 11:22:19 +0800

    feat(monaco-editor): 增强Monaco编辑器功能与配置
    引入useRef钩子，优化Monaco编辑器的配置，添加了新的编辑器实例，增强了自动补全、提示功能和验证选项，以提升用户体验和编辑器的灵活性。

--- src/monaco-editor/index.jsx
+++ src/monaco-editor/index.jsx
@@ -1,5 +1,5 @@
 import * as monaco from 'monaco-editor';
-import React from 'react';
+import React, { useEffect,useRef } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 const sampleValue=[
   {
@@ -85,93 +85,9 @@ const sampleValue=[
   }
 ]
 
-monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
-  validate: true,
-  schemas: [
-    {
-      uri: "http://myschema/abi-schema.json",
-      fileMatch: ["*"],
-      schema: {
-        type: "array",
-        items: {
-          type: "object",
-          required: ["type"],
-          properties: {
-            anonymous: { 
-              type: "boolean",
-              description: "是否为匿名事件，false表示该事件是链上可识别的"
-            },
-            inputs: {
-              type: "array",
-              description: "输入参数，要求参数顺序要和合约中参数顺序一致",
-              items: {
-                type: "object",
-                properties: {
-                  indexed: { 
-                    type: "boolean",
-                    description: "表示该参数是索引参数，可以被用于过滤事件"
-                  },
-                  internalType: { 
-                    type: "string",
-                    description: "内部类型，表示数据类型，如uint256、address等"
-                  },
-                  name: { 
-                    type: "string",
-                    description: "参数名称"
-                  },
-                  type: { 
-                    type: "string",
-                    description: "参数类型，如uint256、address等"
-                  }
-                }
-              }
-            },
-            name: { 
-              type: "string",
-              description: "函数或事件名称"
-            },
-            describe: { 
-              type: "string",
-              description: "函数或事件的描述"
-            },
-            outputs: {
-              type: "array",
-              description: "输出参数，要求参数顺序要和合约中参数顺序一致",
-              items: {
-                type: "object",
-                properties: {
-                  internalType: { 
-                    type: "string",
-                    description: "内部类型，表示数据类型"
-                  },
-                  name: { 
-                    type: "string",
-                    description: "参数名称"
-                  },
-                  type: { 
-                    type: "string",
-                    description: "参数类型"
-                  }
-                }
-              }
-            },
-            stateMutability: { 
-              type: "string", 
-              enum: ["view", "pure", "nonpayable", "payable"],
-              description: "函数的状态可变性：view(只读)、nonpayable(可修改状态但不接收以太币)、pure(不读取或修改状态)、payable(可接收以太币)"
-            },
-            type: { 
-              type: "string", 
-              enum: ["function", "event", "constructor", "fallback", "receive"],
-              description: "类型：function(函数)、event(事件)、constructor(构造函数)、fallback(回退函数)、receive(接收函数)"
-            }
-          }
-        }
-      }
-    }
-  ]
-});
+
 function AntGraph() {
+  const editorRef = useRef(null);
   return <div style={{
     display: "flex"
   }}>
@@ -181,17 +97,135 @@ function AntGraph() {
       language="json"
       theme="vs-dark"
       value={JSON.stringify(sampleValue, null, 2)}
+      editorDidMount={(editor, monaco) => {
+        const model = editor.getModel();
+        monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+          validate: true,
+          schemas: [
+            {
+              uri: "http://myschema/abi-schema.json",
+              fileMatch: [model.uri.toString()],
+              schema: {
+                type: "array",
+                items: {
+                  type: "object",
+                  required: ["type"],
+                  properties: {
+                    anonymous: { 
+                      type: "boolean",
+                      description: "是否为匿名事件，false表示该事件是链上可识别的"
+                    },
+                    inputs: {
+                      type: "array",
+                      description: "输入参数，要求参数顺序要和合约中参数顺序一致",
+                      items: {
+                        type: "object",
+                        properties: {
+                          indexed: { 
+                            type: "boolean",
+                            description: "表示该参数是索引参数，可以被用于过滤事件"
+                          },
+                          internalType: { 
+                            type: "string",
+                            description: "内部类型，表示数据类型，如uint256、address等"
+                          },
+                          name: { 
+                            type: "string",
+                            description: "参数名称"
+                          },
+                          type: { 
+                            type: "string",
+                            description: "参数类型，如uint256、address等"
+                          }
+                        }
+                      }
+                    },
+                    name: { 
+                      type: "string",
+                      description: "函数或事件名称"
+                    },
+                    describe: { 
+                      type: "string",
+                      description: "函数或事件的描述"
+                    },
+                    outputs: {
+                      type: "array",
+                      description: "输出参数，要求参数顺序要和合约中参数顺序一致",
+                      items: {
+                        type: "object",
+                        properties: {
+                          internalType: { 
+                            type: "string",
+                            description: "内部类型，表示数据类型"
+                          },
+                          name: { 
+                            type: "string",
+                            description: "参数名称"
+                          },
+                          type: { 
+                            type: "string",
+                            description: "参数类型"
+                          }
+                        }
+                      }
+                    },
+                    stateMutability: { 
+                      type: "string", 
+                      enum: ["view", "pure", "nonpayable", "payable"],
+                      description: "函数的状态可变性：view(只读)、nonpayable(可修改状态但不接收以太币)、pure(不读取或修改状态)、payable(可接收以太币)"
+                    },
+                    type: { 
+                      type: "string", 
+                      enum: ["function", "event", "constructor", "fallback", "receive"],
+                      description: "类型：function(函数)、event(事件)、constructor(构造函数)、fallback(回退函数)、receive(接收函数)"
+                    }
+                  }
+                }
+              }
+            }
+          ]
+        })
+      }}
       options={{
+        minimap: { enabled: false },
+        lineNumbers: true,
         quickSuggestions: true, // 自动补全
         autoIndent: true,
         automaticLayout: true,
         validate: true,
+        folding: true,
         hover: {
           enabled: true,
         },
         suggest: {
           insertMode: "insert",
-          snippetsPreventQuickSuggestions: false,
+          showInlineDetails: true,
+          showDetails: true,
+          preview: true,
+          previewMode: 'prefix',
+          maxVisibleSuggestions: 12,
+        },
+      }}
+    />
+
+<MonacoEditor
+      width="800px"
+      height="700px"
+      language="json"
+      theme="vs-dark"
+      value={JSON.stringify(sampleValue, null, 2)}
+      options={{
+        lineNumbers: true,
+        quickSuggestions: true, // 自动补全
+        autoIndent: true,
+        automaticLayout: true,
+        validate: true,
+        folding: true,
+        hover: {
+          enabled: true,
+        },
+        suggest: {
+          insertMode: "insert",
           showInlineDetails: true,
           showDetails: true,
           preview: true,
