e967e1488a62d4f268cf63552179081e561bce5d
alanhe <qianghe421@gmail.com>
2025-05-13 22:49:12 +0800

    feat: 添加价格监控功能并优化表单渲染

--- src/pages/form/footer.jsx
+++ src/pages/form/footer.jsx
@@ -1,7 +1,12 @@
 import { Print } from "./print";
 import { Card } from "tea-component";
+import { useFormContext, useWatch } from "react-hook-form";
 
 export function ProductFooter() {
+  const {control, watch} = useFormContext();
+  // const priceWatch = useWatch({control, name: 'price'});
+  const priceWatch = watch('price');
+  console.log('render footer', priceWatch);
   return <Card>
     <Card.Header>
       打印表单数据
